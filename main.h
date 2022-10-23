e2b249b7eeed08692e3452f575d0fac97506fa4b
Travis Jack <ondegotravis@gmail.com>
2022-10-23 19:43:03 +0300

    printf commit

--- main.h
+++ main.h
@@ -1,7 +1,27 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * struct special_characters - Structure containing special structures
+ * and their functions
+ * @s: the special function
+ * @f: function pointer
+ */
+typedef struct special_characters
+{
+	char *s;
+	int (*f)(va_list);
+} spec;/* specifiers */
 
 int _printf(const char *format, ...);
+int _putchar(char c);
+int (*check_format(const char *format))(va_list);
+int (*format_check(const char *format))(va_list);
+int print_char(va_list arg_c);
+int print_str(va_list arg_str);
 
-#endif 
+
+#endif
