37e18158164a01cbf833c4dd367ee86640adeb88
IvyLindaK <lindaivykhasoa@gmail.com>
2022-10-27 15:29:11 +0000

    String change

--- function.h
+++ function.h
@@ -33,9 +33,13 @@ int print_str(va_list arg_str)
 	char *str_arg = va_arg(arg_str, char*);
 	int x = 0;
 
+	if (str_arg == NULL)
+		return (-1);
+
 	while (str_arg[x] != '\0')
 	{
-		_putchar(str_arg[x++]);
+		x++;
+		_putchar(str_arg[x]);
 	}
 	return (1);
 }
